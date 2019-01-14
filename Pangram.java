4f40eb171d8ddf96be68462932765c2baab10ea2
Tanonic <noprocraft@gmail.com>
2019-01-14 18:35:47 +0100

    finished pangram

--- Pangram.java
+++ Pangram.java
@@ -5,18 +5,24 @@ import java.lang.*;
 public class Pangram {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(check("The quick brown fox jumps over the lazy dog."));
+        System.out.println(check("You shall not pass!"));
     }
 
     static char[] a = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
-    public boolean check(String sentence){
-        outerlooop:
-        for (int i = 0; i < sentence.length(); i++){
-            for (int j = 0; j <= a.length; j++){
-                if (j == a.length) return false;
-                if (sentence.charAt(i) == a[j] || Character.toUpperCase(sentence.charAt(i)) == a[j] || Character.isAlphabetic(sentence.charAt(i))){
-                    continue outerlooop;
+    public static boolean check(String sentence){
+        outerloop:
+        for(int i = 0; i < a.length; i++){
+            for (int j = 0; j <= sentence.length(); j++){
+                if (j >= sentence.length()){
+                    return false;
+                }
+                if (!Character.isAlphabetic(sentence.charAt(j))){
+                    continue;
+                }
+                if (a[i] == sentence.charAt(j) || sentence.charAt(j) == Character.toUpperCase(a[i])){
+                    continue outerloop;
                 }
             }
         }
